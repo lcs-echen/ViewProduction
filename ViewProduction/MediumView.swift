bc048fa33c5aeda476703100600259844c7b351f
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-11 18:57:48 -0500

    Finished the first easy view

---
+++ ViewProduction/MediumView.swift
@@ -0,0 +1,20 @@
+//
+//  MediumView.swift
+//  ViewProduction
+//
+//  Created by Evelyn Chen on 2023-01-11.
+//
+
+import SwiftUI
+
+struct MediumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MediumView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediumView()
+    }
+}
