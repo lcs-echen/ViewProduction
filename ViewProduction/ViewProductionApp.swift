0d07b71e708972ef981db49e0f08f4aa4986a081
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-11 18:46:59 -0500

    Initial Commit

---
+++ ViewProduction/ViewProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewProductionApp.swift
+//  ViewProduction
+//
+//  Created by Evelyn Chen on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct ViewProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
